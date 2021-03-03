6b60b6972ef2438aa32a13b721bdfbd49226376a
marlenescobarl <marlenescobarlima11@gmail.com>
2021-03-02 22:32:23 -0600

    se agregÃó el menú principal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Menu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("________________________________________");
+            Console.WriteLine("|   MENU DE OPE OPERACIONES BASICAS!   |");
+            Console.WriteLine("|______________________________________|");
+
+            boolean salir = false;
+
+            while()
+            {
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Modulo");
+                Console.WriteLine("6. Salir");
+                
+
+
+            }
         }
     }
 }
