7757cc528e8107fb93979cd07205a1a815466aec
marlenescobarl <marlenescobarlima11@gmail.com>
2021-03-04 20:13:59 -0600

    se hicieron arreglos respecto al menu

--- Program.cs
+++ Program.cs
@@ -6,24 +6,64 @@ namespace Menu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("________________________________________");
-            Console.WriteLine("|   MENU DE OPE OPERACIONES BASICAS!   |");
-            Console.WriteLine("|______________________________________|");
+            bool salir = false;
 
-            boolean salir = false;
-
-            while()
+            while (!salir)
             {
+                Console.WriteLine("________________________________________");
+                Console.WriteLine("|     MENU DE OPERACIONES BASICAS!     |");
+                Console.WriteLine("|______________________________________|");
+
+                Console.WriteLine("Ingrese el primer numero: ");
+                float number1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el segundo numero: ");
+                float number2 = float.Parse(Console.ReadLine());
+                
+                Console.WriteLine("____________________________________________");
+
                 Console.WriteLine("1. Suma");
                 Console.WriteLine("2. Resta");
                 Console.WriteLine("3. Multiplicacion");
                 Console.WriteLine("4. Division");
                 Console.WriteLine("5. Modulo");
                 Console.WriteLine("6. Salir");
+                Console.WriteLine();
+                String op = Console.ReadLine();
+
+                switch (op )
+                {
+                    case "1": 
+                        float resultado1 = number1 + number2;
+                        Console.WriteLine("El resultado es: " + resultado1);
+                        break;
+                    case "2":
+                        float resultado1 = number1 - number2;
+                        Console.WriteLine("El resultado es: " + resultado1);
+                        break;
+                    case "3":
+                        float resultado1 = number1 * number2;
+                        Console.WriteLine("El resultado es: " + resultado1);
+                        break;
+                    case "4":
+                        float resultado1 = number1 / number2;
+                        Console.WriteLine("El resultado es: " + resultado1);
+                        break;
+                    case "5":
+                        float resultado1 = number1 % number2;
+                        Console.WriteLine("El resultado es: " + resultado1);
+                        break;
+                    case "6":
+                        salir = true; 
+                        break;                                    
+                    default:
+                         Console.WriteLine("La opcion ingresada no es valida ");
+                        break;
+                    
+                }
                 
-
-
             }
+
         }
     }
 }
